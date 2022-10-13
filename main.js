01c1b9a00cfe6178fd4902dd513626d171d66b25
Ryan7AIT <aitmechedalryan7@gmail.com>
2022-10-13 22:16:19 +0100

    improve design

--- main.js
+++ main.js
@@ -33,10 +33,18 @@ var sdc = document.getElementById("sumbiteDeCrypt"); // button to decrypt a deme
 methods
 
 var inp = document.getElementById("searchTxt"); // the original message
+var inpA = document.getElementById("valueOfa"); // the original message
+var inpB = document.getElementById("valueOfb"); // the original message
+var inpN = document.getElementById("valueOfn"); // the original message
+
 var inpl = document.getElementById("methods");
 var result = document.getElementById("resultTxt"); // the descryptes label
+var originalWord = document.getElementById("originalWord"); // the original label
+
 
 var decryptedMessage = ""
+originalWord.value = ""
+
 
 result.value = decryptedMessage
 
@@ -47,14 +55,14 @@ result.value = decryptedMessage
 
 function yesnoCheck() {
     if (inpl.value == 1) {
-        document.getElementById("valueOfn").classList.add("hidden")
+        document.getElementById("invalueOfn").classList.add("hidden")
         document.getElementById("valueOfaAndb").classList.remove("hidden")
     } else if (inpl.value == 3) {
         document.getElementById("valueOfaAndb").classList.add("hidden")
-        document.getElementById("valueOfn").classList.remove("hidden")
+        document.getElementById("invalueOfn").classList.remove("hidden")
     } else {
         document.getElementById("valueOfaAndb").classList.add("hidden")
-        document.getElementById("valueOfn").classList.add("hidden")
+        document.getElementById("invalueOfn").classList.add("hidden")
     }
 }
 
@@ -69,11 +77,11 @@ sc.addEventListener('click', function() {
         
 
     }else if(inpl.value == 1) {
-        decryptedMessage =  affine(inp.value);
+        decryptedMessage =  affine(inp.value,inpA.value,inpB.value);
     } else if(inpl.value == 2) {
         decryptedMessage =  decalage(inp.value);
     } else {
-        decryptedMessage =  cesar(inp.value);
+        decryptedMessage =  cesar(inp.value,inpN.value);
 
     }
 
@@ -84,19 +92,65 @@ sc.addEventListener('click', function() {
 
 })
 
+sdc.addEventListener('click', function() {
+    // console.log("you choosed the methode: ");
+    // console.log(allMethods[parseInt(inpl.value)]);
+    // console.log("you choosed the text: ");
+    // console.log(inp.value);
+
+    if(inpl.value == 0) {
+        decryptedMessage =  dmiroir(inp.value);
+        
+
+    }else if(inpl.value == 1) {
+        decryptedMessage =  daffine(inp.value,inpA.value,inpB.value);
+    } else if(inpl.value == 2) {
+        decryptedMessage =  ddecalage(inp.value);
+    } else {
+        decryptedMessage =  dcesar(inp.value,inpN.value);
+
+    }
+
+    
+
+    originalWord.value = decryptedMessage
+
+
+})
+
 
 function miroir(message) {
     return "this is the resulat of crypting " +  message + " using MIROIR method"
 }
 
-function affine(message) {
-    return "this is the resulat of crypting " +  message + " using MIROIR method with a and b = " 
+function affine(message,a,b) {
+    console.log(a);
+    return "this is the resulat of crypting " +  message + " using affine| a and b:  " + a + "," + b
 }
 
 function decalage(message) {
     return "this is the resulat of crypting " +  message + " using DECALAGE  method"
 }
 
-function cesar(message) {
-    return "this is the resulat of crypting " +  message + " using MIROIR method using a range of : " 
+function cesar(message,n) {
+    return "this is the resulat of crypting " +  message + " using MIROIR method using a range of : " + n
+}
+
+
+
+function dmiroir(message) {
+    return "this is the resulat of decrypting " +  message + " using MIROIR method"
+}
+
+function daffine(message,a,b) {
+    console.log(a);
+    return "this is the resulat of decrypting " +  message + " using affine| a and b:  " + a + "," + b
+}
+
+function ddecalage(message) {
+    return "this is the resulat of decrypting " +  message + " using DECALAGE  method"
+}
+
+function dcesar(message,n) {
+    return "this is the resulat of decrypting " +  message + " using MIROIR method using a range of : " + n
 }
